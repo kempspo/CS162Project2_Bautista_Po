a3290b27a8580c5e79c68d1409d79b9411c4047c
kempspo <kempspo@gmail.com>
2018-04-29 16:44:58 +0800

    final

--- CS162Project2_Bautista_Po.cpp
+++ CS162Project2_Bautista_Po.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+//checks for unfinished processes
+int checkBool(int check[]){
+	for(int i = 0; i < numprocess; i++){
+		if(!check[i])
+			return 0;
+	}
+	return 1;
+}
+
 int main(int argc, char* argv[])
 {
 	string sfile = argv[1];
@@ -18,20 +27,81 @@ int main(int argc, char* argv[])
 
 	int testcase;
 	int numprocess, numresource;
-	int resource[];
-	int have[], need[];
+	int check;
+	int resource[numresource];
+	int have[numprocess][numresource], need[numprocess][numresource];
 
-	cin >> testcase;
+	/** looping through file for input **/
+	file >> testcase;
 	for(int i = 0; i < testcase; i++){
-		cin >> numprocess >> numresource;
+		file >> numprocess >> numresource;
+		//gets available resources
 		for(int j = 0; j < numresource; j++){
-			cin >> resource[j]; 
+			file >> resource[j]; 
 		}
-		for(int x = 0; x < (numprocess * numresource); x++){
-			cin >> have[x];
+		//gets resources each process has
+		for(int j = 0; j < numprocess; j++){
+			check[i] = 0;
+			for(int k = 0; k < numresource; k++){
+				file >> have[j][k];
+			}
 		}
-		for(int x = 0; x < (numprocess * numresource); x++){
-			cin >> need[x];
+		//gets resources each process still needs
+		for(int j = 0; j < numprocess; j++){
+			for(int k = 0; k < numresource; k++){
+				file >> need[j][k];
+			}
 		}
-	} 
+	}
+
+	/** start doing stuff **/
+	int counter = 0;
+	int deadlock = 1;
+	int finish[numprocess];
+	int doneCount = 0;
+
+	// just check until all processes are done
+	while(!checkBool(check)){
+		for( int i = 0; i < numprocess; i++){
+			deadlock = 1;
+			if(check[i] == 0){
+				for(int j = 0; j < numresource; j++){
+					//check if resource can be given to process by available
+					if(need[i][j] <= available[j]){
+						counter++; // add to counter if allocated
+					}
+				}
+
+				// if equal then it just means can already be filled
+				if(counter == numresource)
+				{
+					check[i] = 1;
+					counter = 0;
+					deadlock = 0;
+
+					//allocate resource to process
+					for(int k = 0; k < numresource; k++)
+					{
+						available[k] += have[i][k];
+					}
+
+					finish[doneCount] = i + 1;
+					doneCount++;
+					//break;
+				}				
+			}
+			counter = 0;
+		}
+	}	
+		if(deadlock){
+			cout << "Deadlock occured\n";
+			break;
+		}
+		if(!deadlock){
+			cout << "Process Request Order:\n";
+			for(int i = 0; i < numprocess; i++){
+				cout << finish[i];
+			}
+			cout << "\n";
+		}
 }
